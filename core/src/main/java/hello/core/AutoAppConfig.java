d9bef7a0c4f4b57683d729fbdb2286aec703a451
codingNoob12 <ilhyeon0326@gmail.com>
2024-04-28 16:34:54 +0900

    #68 - 컴포넌트 스캔 basePackageClasses 변경
    `basePackageClasses`를 지정해 컴포넌트 스캔 위치를 변경할 수 있음을 확인

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.FilterType;
 @Configuration
 @ComponentScan(
     basePackages = "hello.core.member",
+    basePackageClasses = AutoAppConfig.class,
     excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
